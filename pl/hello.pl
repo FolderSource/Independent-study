d86bc8dcda3419e1119e8ac8909225b17899716a
Zeyar Win <zxw6805@rit.edu>
2016-09-05 00:08:09 -0400

    okay

--- pl/hello.pl
+++ pl/hello.pl
@@ -11,5 +11,22 @@ use warnings;
 # print a message
 #
 print "Hello, World!\n";
+# use Getopt::Long; GetOptions
+my $file = "temp.txt";
 
+unless(open FILE, '>'.$file){
+	die "\nUnable to create $file\n";
+}
 
+    my $message = <<"END_MESSAGE";
+        Dear bro,
+    
+        this is a message I plan to send to you.
+    
+        regards
+          the Perl Maven
+END_MESSAGE
+    print $message;
+
+    print FILE $message;
+close FILE;
